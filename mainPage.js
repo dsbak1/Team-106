f26b5b8b44ea4089c07e218386244b15a1d22485
dsbak1 <dsbak1@student.monash.edu>
2016-05-26 00:24:52 +1000

    Add files via upload

--- mainPage.js
+++ mainPage.js
@@ -16,12 +16,47 @@ function viewLocation(locationName)
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-function viewLocation(locationName)
+function main() {
+  	var ul = document.getElementByID("locationList");
+  	
+  	for (var index = 0; index < locationWeatherCache.length(); index++) {
+      	var location = locationWeatherCache.locationAtIndex(index);
+      	
+      	var li = document.createElement("li");
+      	li.className = "mdl-list__item mdl-list__item--two-line";
+      	li.onclick = viewLocation(location.nickname);
+      	ul.appendChild(li);
+      
+      	var firstSpan = document.createElement("span");
+      	firstSpan.className = "mdl-list__item-primary-content";
+      	li.appendChild(firstSpan);
+      
+      	var img = document.createElement("img");
+      	img.className = "mdl-list__item-icon list-avatar";
+      	img.src = "images/loading.png";
+      	firstSpan.appendChild(img)
+        
+        var secondSpan = document.createElement("span");
+      	secondSpan.innerHTML = location.nickname;
+      	firstSpan.appendChild(secondSpan);
+
+        var thirdSpan = document.createElement("span");
+      	thirdSpan.className = "mdl-list__item-sub-title";
+      	thirdSpan.innerHTML = "Weather Summary";
+      	firstSpan.appendChild(thirdSpan);
+    }
+}()
+
+/*var information = {
+    // [name; nickname; latitude; longitude]
+}
+
+function displayInfo()
 {
-    // Save the desired location to local storage
-    localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
-    // And load the view location page.
-    location.href = 'viewlocation.html';
+    for (var i = 0; i <=usefulInformation.length; i++)
+        {
+            return usefulInformation[i]
+        }
 }
 
 var longLatTest =
@@ -63,4 +98,4 @@ function jsonpRequest(url, params)
    		document.body.appendChild(script);
 	}
 
-jsonpRequest("https://api.forecast.io/forecast/", entry)
+jsonpRequest("https://api.forecast.io/forecast/", entry)*/
